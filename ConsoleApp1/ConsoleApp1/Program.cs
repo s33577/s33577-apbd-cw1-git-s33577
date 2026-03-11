7295ee4970ef821241098d700eebf1b3346f8ad4
Andrew Kotowiecki <s33577@pjwstk.edu.pl>
2026-03-11 11:13:20 +0100

    Task 2 - Create PSVM

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("APBD task 1");
+class Program {
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("APBD task 1");
+    }
+    
+}
