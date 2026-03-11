4a44ba9042626d5be38132bb0f1a378fb70938aa
Andrew Kotowiecki <s33577@pjwstk.edu.pl>
2026-03-11 11:23:56 +0100

    Task 4 - CalculateAverage Method
    ## Description
    Created CalculateAverage that takes a array of numbers and returns the average

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,6 +4,26 @@ class Program {
     public static void Main(String[] args)
     {
         Console.WriteLine("APBD task 2" + " Andrew Kotowiecki");
+        
+    }
+    static double CalculateAverage(int[] values)
+    {
+        if (values.Length == 0)
+        {
+            throw new Exception("Empty array");
+        }
+
+        if (values == null)
+        {
+            throw new Exception("Null array");
+        }
+        
+        long sum = 0;
+        for (int i = 0; i < values.Length; i++)
+        {
+            sum += values[i];
+        }
+        return (double)sum / values.Length;
     }
     
 }
